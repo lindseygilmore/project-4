ebf05cfbe4f0adb7b6cb18b8b227199178101781
lindseygilmore <lindseygilmore21@gmail.com>
2017-08-09 14:56:50 -0500

    questions hiding properly

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,12 +2,34 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
+class Restaurant{
+	name: string;
+	address: string;
+	phone: string;
+	price: string;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 	answers: {};
+	// questions: {
+	// 	question: "Which location do you want to eat?",
+	// 	options: ["Loop", "River North", "Old Town", "Wicker Park"],
+	// 	answer: ""
+	// },
+	// {
+	// 	question: "What type of cuisine do you want to eat?",
+	// 	options: ["Sushi", "Burgers", "Tacos", "Pizza"],
+	// 	answer: ""
+	// },
+	// {
+	// 	question: "What type of cuisine do you want to eat?",
+	// 	options: ["Sushi", "Burgers", "Tacos", "Pizza"],
+	// 	answer: ""
+	// },
 	showQuestionOne: boolean = true;
 	showQuestionTwo: boolean = false;
 	showQuestionThree: boolean = false;
@@ -19,20 +41,41 @@ export class HomePage {
 
 	}
 
-		goToQuestionTwo(){
-			this.showQuestionTwo = true;
-			console.log("clicked");
+		goToQuestionTwo() {
+		    let x = document.getElementById('questionOne')
+
+		    if (x.style.display === 'none') {
+		        x.style.display = 'block'
+		    } else {
+		        x.style.display = 'none'
+		    }
 		}
 
-		goToQuestionThree(){
-			this.showQuestionOne = false;
-			this.showQuestionTwo = false;
-			this.showQuestionThree = true;
+		goToQuestionThree() {
+		    let x = document.getElementById('questionTwo')
+
+		    if (x.style.display === 'none') {
+		        x.style.display = 'block'
+		    } else {
+		        x.style.display = 'none'
+		    }
 		}
 
-		generateRestaurants(){
 
-		}
+		// goToQuestionTwo(){
+		// 	this.showQuestionTwo = true;
+		// 	console.log("clicked");
+		// }
+
+		// goToQuestionThree(){
+		// 	this.showQuestionOne = false;
+		// 	this.showQuestionTwo = false;
+		// 	this.showQuestionThree = true;
+		// }
+
+		// getRestaurants(){
+
+		// }
 
 
 	
