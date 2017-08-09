ed0df5ca4d0a923b0d39e08c91caf44dc29f55ed
lindseygilmore <lindseygilmore21@gmail.com>
2017-08-08 21:24:55 -0500

    myApp questions populated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,12 @@ export class HomePage {
 
   }
 
+   //  	onSearch(){
+  	// 	console.log('clicked');
+  	// 	let apiKey = '00205583460d0e51c5b2bdc03f62a7c7';
+  	// 	this.http.get('http://food2fork.com/api/search?key=' + apiKey + '&q=').subscribe(response => {
+  	// 		console.log(response)
+  	// 	})
+  	// }
+
 }
