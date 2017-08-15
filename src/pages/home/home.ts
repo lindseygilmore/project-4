cc77904367ffcdd0176327fd3c2aa4b9867c6bc2
lindseygilmore <lindseygilmore21@gmail.com>
2017-08-15 09:34:56 -0500

    yelp api working tuesday

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,19 +2,29 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Http, Response } from '@angular/http';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 
+
 export class HomePage {
 	answers = [];
 
+	// showIntro: boolean = true;
 	showQuestionOne: boolean = true;
 	showQuestionTwo: boolean = false;
 	showQuestionThree: boolean = false;
 	showSearchCriteria: boolean = false;
 	showRestaurants: boolean = false;
+
+	// locationNamesAndCoords = {
+	// 	westLoop: [41.8825, -87.6447],
+	// 	riverNorth: [41.8924, -87.6341],
+	// 	wickerPark: [41.9088, -87.6796],
+	// 	Loop: [41.8786, -87.6251]
+	// }
 
 
   constructor(public navCtrl: NavController, private http: Http) {
@@ -22,7 +32,10 @@ export class HomePage {
 
 	}
 
-		
+		// goToQuestionOne() {
+		// 	this.showIntro = false;
+		// 	this.showQuestionOne = true;
+		// }
 
 		goToQuestionTwo() {
 			console.log(this.answers)
@@ -57,10 +70,8 @@ export class HomePage {
 
 			this.http.get('http://localhost:3000/eat').subscribe(response => {
 				console.log(response.json())
-	
+
 			});
-
-
 
 	
 
